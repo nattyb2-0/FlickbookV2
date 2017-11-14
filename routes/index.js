152a35bd6e47c8aaf4607d73a60cf99c3c6e74c6
Natty Davis <natty.davis79@gmail.com>
2017-11-14 07:34:35 -0500

    added model structure for all routes

--- routes/index.js
+++ routes/index.js
@@ -15,17 +15,3 @@ const router = express.Router()
 module.exports = router;
 
 
-// router.route('/')
-//       .get(async(req,res,next)=>{
-//        const result= await database.any(`select * from users`)
-//        console.log(result)
-//        // res.send('fu')
-//         if(result){
-//           res.status(200).json({result})
-//         }
-
-//         next()
-//         })
-//       .post((req,res,next)=>{
-//         res.send('need to add db model to add user')
-//       })
